64bc5a9ec0da8e34721f267fefd84bf5d75069c5
oscar1234456 <chen.oscar@hotmail.com>
2019-12-22 15:24:20 +0800

    [add]Add some comment

--- das_daa_simulation/AddTwo.asm
+++ das_daa_simulation/AddTwo.asm
@@ -8,8 +8,8 @@ include Irvine32.inc
 .code
 
 main PROC
-    mov al, 48h
-	sub al, 35h
+	mov al, 32h
+	sub al, 39h
     call myDAS
 	call DumpRegs
 
@@ -19,40 +19,40 @@ main ENDP
 
 myDAS PROC
 	mov bl, al
-	lahf
+	lahf ;將EFlag暫存器低8bit送進AH(SF,ZF,Res.,AF,Res.,PF,Res.,CF)
 	mov cl, ah
-	and bl, 00001111b
-	and cl, 00010000b
-	shr cl,4
+	and bl, 00001111b	;藉由bl捕捉al的低4bit
+	and cl, 00010000b	;藉由cl得知AuxCarry的狀況
+	shr cl,4 ;把上一步之bit推至第一位
 
-	.IF bl > 9  ||  cl == 1
+	.IF bl > 9  ||  cl == 1	;依照講義logic進行實作(al(lo)>0或AuxCarry == 1)
 		sub al, 6
 		pushf
-		or ah,00010000b
+		or ah,00010000b;調整AuxCarry = 1
 		popf
-		sahf
+		sahf ;將AH的8bit送至Eflag
 	.ELSE
 		pushf
-		or ah,00000000b
+		or ah,00000000b; 調整AuxCarry = 0
 		popf
-		sahf
+		sahf ;將AH的8bit送至Eflag
 	.ENDIF
 
-	lahf
+	lahf ; 再一次將EFlag暫存器低8bit送進AH(因為line29 sub會再一次影響)
 	mov cl, ah
 	and cl, 00000001b
 
-	.IF al > 9Fh || cl == 1
+	.IF al > 9Fh || cl == 1 ; 依照講義logic進行實作(al > 0或Carry == 1)
 		sub al, 60h
 		pushf
-		or ah, 00000001b
+		or ah, 00000001b ; 調整Carry = 1
 		popf
-		sahf
+		sahf ; 將AH的8bit送至Eflag
 	.ELSE
 		pushf
-		or ah, 00000000b
+		or ah, 00000000b ; 調整Carry = 0
 		popf
-		sahf
+		sahf ; 將AH的8bit送至Eflag
 	.ENDIF
 
 ret
